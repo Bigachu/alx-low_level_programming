2c216f653c69c5304571abd3b7a47d49aa38d29b
bigachu <briegach@gmail.com>
2023-06-15 13:30:08 -0700

    Modify

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  * Description: A program that writes function
- * Return 1
+ * Return: 1
  */
 int main(void)
 char [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
