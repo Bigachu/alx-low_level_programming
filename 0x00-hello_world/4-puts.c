dc01a86f6bab7609381ce6139aa05c4624b3e170
bigachu <briegach@gmail.com>
2023-06-15 10:18:40 -0700

    Create  first C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Aprogram that prints a command followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
