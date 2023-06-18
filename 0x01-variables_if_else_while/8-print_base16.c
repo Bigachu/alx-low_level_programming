af694dc95a7e8041a0f8ccc16a4654d21b8add18
bigachu <briegach@gmail.com>
2023-06-17 17:16:29 -0700

    Modify

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,15 +7,15 @@
 int main(void)
 {
 	int n;
-	int x;
+	int y;
 
 	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
 	}
-	for (x = 97; x <= 102; x++)
+	for (y = 97; y <= 102; y++)
 	{
-		putchar(x);
+		putchar(y);
 	}
 	putchar('\n')
 	return (0);
