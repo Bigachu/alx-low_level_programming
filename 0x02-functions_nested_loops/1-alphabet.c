cdbe2e0140fc1ac0a473dbbc9a4dd9ad3bea3198
bigachu <briegach@gmail.com>
2023-06-21 02:12:31 -0700

    100-times_table

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,11 @@
  * print_alphabet - lowercase
  * Return: void
  */
+
 void print_alphabet(void)
 {
-	char ch;
-	for (ch = 'a'; ch <= 'z'; ch++)
-		_putchar(ch);
-	_putchar('\n');
-}
-	
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}	
